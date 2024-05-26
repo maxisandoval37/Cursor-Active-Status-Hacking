c83f2bd5ce721a0819ec72b4d06834d9e198402c
Maxi Sandoval <maxisandoval98@gmail.com>
2024-05-26 18:15:32 -0300

    generic name app and icon

--- CursorStatusTeamsHacking/obj/Release/net5.0-windows/CursorActiveStatusHacking.AssemblyInfo.cs
+++ CursorStatusTeamsHacking/obj/Release/net5.0-windows/CursorActiveStatusHacking.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("CursorActiveStatusHacking")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("System64")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("CursorActiveStatusHacking")]
-[assembly: System.Reflection.AssemblyTitleAttribute("CursorActiveStatusHacking")]
+[assembly: System.Reflection.AssemblyProductAttribute("System64")]
+[assembly: System.Reflection.AssemblyTitleAttribute("System64")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
